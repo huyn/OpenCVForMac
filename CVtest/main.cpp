866d78d4e96731750fbfa0316ee44ab43ff04114
huyn <pumpkinhuyn@gmail.com>
2017-07-05 10:04:32 +0800

    Initial Commit

---
+++ CVtest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CVtest
+//
+//  Created by 胡耀楠 on 2017/7/5.
+//  Copyright © 2017年 胡耀楠. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
